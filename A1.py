d2aac81cafbe817edd5f81d5267304dfd4204236
lm2-me <52729991+lm2-me@users.noreply.github.com>
2022-02-15 17:11:03 +0100

    point cloud height

--- A1.py
+++ A1.py
@@ -1,16 +1,32 @@
-#Main
+
+
 
 #Import Point Cloud
 
+#Get object height for each point cloud height
+def allObjectHeights(pointCloudDirectory):
+    for file in pointCloudDirectory:
+        #read file
+        #assign data from current file to currentPointCloud variable
+        currentPointCloud = [[1,4,8], [3,4,5], [2,4,7]]
+        objectHeight(currentPointCloud)
 
-#Get feature 1
+#Get feature 1: Height
+def objectHeight(currentPointCloud):
+    maxZ = 0
+    length = len(currentPointCloud)
 
+    for point in currentPointCloud:
+        if point[2] > maxZ:
+            maxZ = point[2]
+        #maxZ now largest Z
 
+    height = maxZ
+    print(height)
+    return height
 
-# seb new lines
+#Get feature 2: Vertical Slices
 
-
-#Get feature 2
 
 #Get feature 3
 
@@ -19,3 +35,10 @@
 #plot features against each other
 
 #implement clustering
+
+#Main
+if __name__ == "__main__":
+    #function importing point clouds
+    # assign to pointCloudDirectory variable 
+    pointCloudDirectory = [" "]
+    allObjectHeights(pointCloudDirectory)
