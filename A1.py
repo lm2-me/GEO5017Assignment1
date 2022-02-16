6abaab6fe427cff3900bf1d880444e8d4d100fd0
Sebastian Stripp <69217147+Lundkjaer@users.noreply.github.com>
2022-02-16 07:32:50 +0100

    Loading cloudpoint files into dictionary

--- A1.py
+++ A1.py
@@ -1,6 +1,45 @@
 #Main
 
+
+#Github
+''' git status, git add ., git commit -m "comment about update", git push. --> git pull'''
+
 #Import Point Cloud
+import pandas as pd
+import numpy as np
+print("start program for importing files")
+
+import os
+
+
+cwd = os.getcwd() # get current directory of file
+filewd = (cwd) # tuple to lock original folder location when changing directory
+# print("this is cwd: ", cwd) # check
+# print("this is filewd: ", filewd) # check
+
+pointcloudFolder = filewd + "\pointclouds"
+#print(pointcloudFolder) #To check
+
+os.chdir(pointcloudFolder)
+print("new directory changed to : {0}".format(os.getcwd()))
+
+d = {}
+"""the list of point clouds are being put in a dictionary as numpy arrays - not sure if this is the best.
+I also considered Pandas. Also wasn't sure whether a list of list would be annoying.
+This way we can call them specificaly by name, so that might be the easiest when iterating through them.
+"""
+
+for i in range(500):
+    # print(i)
+    number = "00" + str(i)
+    three_digit = number[-3:]
+    open_array = np.genfromtxt("{0}.xyz".format(three_digit))
+    d["pc{0}".format(three_digit)] = open_array
+
+print(d["pc385"])
+
+
+
 
 
 #Get feature 1
